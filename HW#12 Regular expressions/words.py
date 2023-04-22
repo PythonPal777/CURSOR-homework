0555b27aa9176d8ffce70ec584c0e38a05e27386
Andriy Pal <44066770+PythonPal777@users.noreply.github.com>
2023-04-22 10:50:00 +0300

    Update words.py

--- HW#12 Regular expressions/words.py
+++ HW#12 Regular expressions/words.py
@@ -1,1 +1,8 @@
+text = input("Введіть текст: ")
+word = input("Введіть слово, яке потрібно знайти: ")
 
+count = text.count(word)
+text = text.replace(word, word.upper())
+
+print(f"Кількість входжень слова '{word}': {count}")
+print(f"Замінений текст: {text}")
